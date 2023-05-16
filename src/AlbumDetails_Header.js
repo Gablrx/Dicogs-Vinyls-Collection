cb5553824003c5084d64efc0648e8578ca585d85
Gablrx <97037067+Gablrx@users.noreply.github.com>
2023-05-16 16:04:11 +0200

    Update AlbumDetails_Header.js

--- src/AlbumDetails_Header.js
+++ src/AlbumDetails_Header.js
@@ -6,10 +6,7 @@ export const AlbumDetailsHeader = ({ details }) => {
 
 
         <div className="AlbumDetailsHeader">
-            <div>
-                <h1>{details.title}</h1>
-                <h3>{details.artists[0].name}</h3>
-            </div>
+            <div><h1>{details.title}</h1></div>
             <Link to={`/`}>
                 <IconContext.Provider value={{ color: "tomato", size: "25px", className: "global-class-name" }}>
                     <div className="closeBtn">
